128de664f7ff747b6d96afce47801713f727a120
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-09 11:30:15 +0300

    add status to tasks

--- rails-task-manager/db/schema.rb
+++ rails-task-manager/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809081354) do
+ActiveRecord::Schema.define(version: 20160809111354) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "status"
   end
 
 end
